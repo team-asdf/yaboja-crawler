a844e3b87d6e47302a208e693c08cb4e849068b0
ghyeon0 <xcm1321@gmail.com>
2018-10-29 20:32:00 +0900

    TextRank Algorithm Test

--- dev_blog_crawl/crawl_subicura_com_bs4.py
+++ dev_blog_crawl/crawl_subicura_com_bs4.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import re
+import io
+from text_rank import *
 from markdownify import markdownify as md
 
 
@@ -25,7 +28,25 @@ class Subicura:
             soup = BeautifulSoup(req.text, 'html.parser')
             date = soup.find("time")
             date = self.parse_date(date.contents[0])
-            self.write.writerow([title, "temp", main_url + sub_url, 0, date, "subicura"])
+
+            content_list = soup.find_all("p")
+            text = ""
+            f = io.open("text.txt", mode="w", encoding="utf-8")
+            for each in content_list:
+                text += each.get_text()
+                f.writelines(each.get_text())
+            f.close()
+            tr = TextRank(window=10, coef=1)
+            print('Load...')
+            stopword = set([('있', 'VV'), ('하', 'VV'), ('되', 'VV'), ('없', 'VV')])
+            tr.load(RawTaggerReader('text.txt'), lambda w: w not in stopword and (w[1] in ('NNG', 'NNP', 'VV', 'VA')))
+            print('Build...')
+            tr.build()
+            kw = tr.extract(0.1)
+            for k in sorted(kw, key=kw.get, reverse=True):
+                print("%s\t%g" % (k, kw[k]))
+
+            self.write.writerow([title, text[:200], main_url + sub_url, 0, date, "subicura"])
         self.file.close()
         print("Subicura Crawl End")
 
