bdcd85829be06d506b52a0fd2b0c6218b3707309
Seonggwon Yoon <keyakoto@gmail.com>
2018-11-28 17:33:51 +0900

    Fix to writing in db
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- db_migration.py
+++ db_migration.py
@@ -1,16 +1,16 @@
 import csv
 import pymysql
 
-conn = pymysql.connect(host='localhost',port=3306,user='root',passwd='',db='test',charset='utf8')
+conn = pymysql.connect(host='localhost',port=3306,user='yaboja',passwd='',db='yaboja',charset='utf8')
 cur = conn.cursor()
 
-lst = ["./tech-crawler-ghyeon/main.csv", "./tech-crawler-jungwoo/main.csv"]
+lst = ["./tech-crawler-ghyeon/csv/main.csv", "./tech-crawler-jungwoo/data/merged.csv"]
 
 for each in lst:
     with open(each) as csvfile:
         sp = csv.DictReader(csvfile)
         for row in sp:
-            sql = 'insert into contents(idx, title, content, url, cnt, source, keyword, image, createdAt, priority) values (idx, "%s","%s","%s","%s","%s","%s","%s","%s","%s")'%(str(row['title']),str(row['content']),str(row['url']),str(row['cnt']),str(row['source']),str(row['keyword']),str(row['image'],str(row['createdAt']),str(row['priority'])))
+            sql = 'insert into contents(`title`, `content`, `url`, `cnt`, `source`, `keyword`, `image`, `createdAt`, `priority`) values ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")'% (str(row['title']), str(row['content']), str(row['url']), str(row['cnt']), str(row['source']), str(row['keyword']), str(row['image']), str(row['createdAt']), str(row['priority']))
             cur.execute(sql)
     csvfile.close()
 
